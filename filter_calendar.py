24891e924f26a698db33ae72f2b090c4eec4bbe8
florianopoliz <florian.tristant@insa-lyon.fr>
2025-10-07 16:49:35 +0200

    Update filter_calendar.py

--- filter_calendar.py
+++ filter_calendar.py
@@ -6,29 +6,38 @@ events = re.findall(r"BEGIN:VEVENT.*?END:VEVENT", ics, re.DOTALL)
 
 cleaned_events = []
 for ev in events:
-    if ("LV1" in ev) or ("LV2" in ev) or (":PCO:" in ev and "4GIPCO4" not in ev):
-        continue
-    if ":OPT:" in ev:
+    if ("LV1" in ev) or ("LV2" in ev) or (":PCO:" in ev and "4GIPCO4" not in ev) or ":OPT:" in ev:
         continue
 
     ev = re.sub(r"LOCATION:\d+ - ", "LOCATION:", ev)
 
-    match = re.search(r"::([A-Z]{3,4}):([A-Z]{2,3})::", ev)
-    if match:
-        code, typ = match.groups()
-        if code == "EPS" and typ == "EDT":
-            new_summary = "SUMMARY:EPS"
-        elif code == "SHS" and typ == "EDT":
-            new_summary = "SUMMARY:SHS"
-        elif "4GIPCO4" in ev:
-            new_summary = f"SUMMARY:PCO - {typ}"
-        else:
-            new_summary = f"SUMMARY:{code} - {typ}"
+    if ":SHS:" in ev:
+        if "HU" not in ev:
+            continue
+        new_summary = "SUMMARY:SHS"
         ev = re.sub(r"SUMMARY:.*", new_summary, ev)
+
+    elif ":EPS:" in ev:
+        if "CDS" in ev:
+            new_summary = "SUMMARY:EPS"
+            ev = re.sub(r"SUMMARY:.*", new_summary, ev)
+        else:
+            continue
+
+    else:
+        match = re.search(r"::([A-Z]{3,4}):([A-Z]{2,3})::", ev)
+        if match:
+            code, typ = match.groups()
+            if code == "EPS" and typ == "EDT":
+                continue
+            elif "4GIPCO4" in ev:
+                new_summary = f"SUMMARY:PCO - {typ}"
+            else:
+                new_summary = f"SUMMARY:{code} - {typ}"
+            ev = re.sub(r"SUMMARY:.*", new_summary, ev)
 
     cleaned_events.append(ev)
 
 cleaned_ics = "BEGIN:VCALENDAR\n" + "\n".join(cleaned_events) + "\nEND:VCALENDAR"
-
 with open("cleaned_calendar.ics", "w", encoding="utf-8") as f:
     f.write(cleaned_ics)
